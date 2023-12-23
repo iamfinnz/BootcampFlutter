d7a962dff09592dba69ce4787e7e0de8467de486
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2023-12-23 17:30:35 +0700

    Pengumpulan Tugas 10

--- sanberappflutter/lib/main.dart
+++ sanberappflutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sanberappflutter/Tugas/Tugas10/HomeScreen.dart';
 import 'Tugas/Tugas9/Telegram.dart';
+import 'package:sanberappflutter/Tugas/Tugas10/LoginScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Telegram(),
+      home: const HomeScreen(),
     );
   }
 }
